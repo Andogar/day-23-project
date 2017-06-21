30e053313877ed1d912291749ccd8d9fad27ebb8
Andogar <robert.andoga@gmail.com>
2017-06-21 17:48:15 -0400

    Connected files together, have list working at a basic level

--- public/main.js
+++
@@ -1,3 +0,0 @@
-//see if your files are linking properly
-//this folder is for your front end javascript
-console.log("HELLO FROM THE BROWSER!")
--- server.js
+++ server.js
@@ -1,32 +1,28 @@
 const express = require('express');
 const mustache = require('mustache-express');
+const bodyParser = require('body-parser');
 
-// set our server object into a variable
 const application = express();
 
-// set an engine called "mustache" using our mustache() library
 application.engine('mustache', mustache());
-
-// set our view engine to use the mustache engine
 application.set('view engine', 'mustache');
-
-//set our views to the ./views directory
 application.set('views', './views');
 
-//uncomment this to "serve" your public folder
-// app.use(express.static(__dirname + '/public'));
+application.use(bodyParser.urlencoded());
 
-application.use('/', function(request, response){
-   // basic send response
-   response.send('heyy there');
+application.use('/public', express.static('./public'));
 
-   //uncomment this to verify mustache is working
-   // response.render('index');
+const todos = [
+  "Wash the car"
+];
+
+application.get("/", function (request, response) {
+  response.render('index', { todos: todos });
+});
+
+application.post("/", function (request, response) {
+  todos.push(request.body.todo);
+  response.redirect('/');
 })
 
-const port = 3000
-application.listen(port, function(){
-   //should see these console loggin in the terminal
-   console.log("server is running!");
-   console.log('listening at port: ', port);
-})
+application.listen(3000);
