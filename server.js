3c2e6e25265629252687242837b63efe23f94b48
Andogar <robert.andoga@gmail.com>
2017-06-22 14:48:26 -0400

    Added support for tasks.json so it will store both complete and incomplete tasks in memory

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const mustache = require('mustache-express');
 const bodyParser = require('body-parser');
+const jsonFile = require('jsonfile');
+const todos = require('./public/tasks.json');
+const filePath = './public/tasks.json';
+const fs = require('file-system');
 
 const application = express();
 
@@ -12,7 +16,6 @@ application.use(bodyParser.urlencoded());
 
 application.use('/public', express.static('./public'));
 
-const todos = [];
 var id = 1;
 
 
@@ -22,26 +25,27 @@ application.get("/", function (request, response) {
 
 application.post("/", function (request, response) {
   if (!request.body.todo) {
-    response.render('index', { todos: todos});
+    response.render('index', { todos: todos });
   } else {
     var listItem = { task: request.body.todo, completed: false, id: id++ }
     todos.push(listItem)
 
     response.redirect('/');
   }
-
+  todosJSON = JSON.stringify(todos);
+  fs.writeFile(filePath, todosJSON, function (err) { })
+  response.redirect('/');
 });
 
 application.post('/:id', (request, response) => {
   todos.find(function (todo) {
     if (todo.id == request.params.id) {
       todo.completed = true;
-      return true;
     }
   })
+  todosJSON = JSON.stringify(todos);
+  fs.writeFile(filePath, todosJSON, function (err) { })
   response.redirect('/');
 });
 
 application.listen(3000);
-
-// need jsonfile and fs require(file-system)
